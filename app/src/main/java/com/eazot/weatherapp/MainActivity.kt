7fc50d25d364f55b8f962e9ab0f05898c5970cc2
“[firstname <zotkinae84@gmail.com>
2021-07-29 01:27:58 +0300

    добавила кнопку, потренировалась в создании классов и функций

--- app/src/main/java/com/eazot/weatherapp/MainActivity.kt
+++ app/src/main/java/com/eazot/weatherapp/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.eazot.weatherapp
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.eazot.weatherapp.repository.Repository
+import com.eazot.weatherapp.repository.RepositorySingle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recyclerWeather = findViewById<RecyclerView>(R.id.recyclerWeather)
+        val adapter = WeatherAdapter(RepositorySingle)
+        recyclerWeather.adapter = adapter
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener(object: View.OnClickListener{
+            override fun onClick(v: View?) {
+                Toast.makeText(this@MainActivity,"Найти город",Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 }
--- app/src/main/java/com/eazot/weatherapp/repository/Repository.kt
+++ app/src/main/java/com/eazot/weatherapp/repository/Repository.kt
@@ -4,10 +4,22 @@ import com.eazot.weatherapp.data.Weather
 
 open class Repository: IRepository {
 
+    private val weathers: List<Weather>
+
+    init {
+        weathers = listOf(
+            Weather("Москва", 30),
+            Weather("Санкт-Петербург", 20),
+            Weather("Самара", 35),
+            Weather("Тюмень", 29),
+            Weather("Рязань", 28)
+        )
+    }
     override fun getWeathers(): List<Weather> {
-        TODO("Not yet implemented")
+        return weathers
     }
 }
+
 interface IRepository{
     fun getWeathers():List<Weather>
 }
